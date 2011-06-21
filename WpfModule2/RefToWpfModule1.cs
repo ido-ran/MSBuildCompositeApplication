b3304efd4dc3cd4915be5ab1ab9a7b8c32639add
Ido Ran <ido.ran@gmail.com>
2011-06-21 12:14:57 +0300

    build with MSBuild task in AfterCompile

---
+++ WpfModule2/RefToWpfModule1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WpfModule1;
+
+namespace WpfModule2 {
+  public class RefToWpfModule1 {
+    CustomControl1 d = new CustomControl1();
+  }
+}
